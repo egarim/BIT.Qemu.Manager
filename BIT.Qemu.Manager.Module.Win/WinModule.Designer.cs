4ab6c974105d1960389d52cac5489d0899ae593a
Jose Manuel Ojeda <manu_ojeda@hotmail.com>
2020-02-15 08:03:29 -0800

    Add project files.

---
+++ BIT.Qemu.Manager.Module.Win/WinModule.Designer.cs
@@ -0,0 +1,35 @@
+﻿namespace BIT.Qemu.Manager.Module.Win {
+    partial class ManagerWindowsFormsModule {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            // 
+            // ManagerWindowsFormsModule
+            // 
+            this.RequiredModuleTypes.Add(typeof(BIT.Qemu.Manager.Module.ManagerModule));
+            this.RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule));
+        }
+
+        #endregion
+    }
+}
---
+++ BIT.Qemu.Manager.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Language="C#" CodeBehind="Global.asax.cs" Inherits="BIT.Qemu.Manager.Web.Global"%>
